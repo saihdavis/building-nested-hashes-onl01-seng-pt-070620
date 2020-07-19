f5662bb4696d8f208e96cf67705b4ab1ba6fbda8
Saida Davis <saihdavis@gmail.com>
2020-07-19 03:30:59 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,5 +1,5 @@
 def bonus
-  epic_tragedy = {
+epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -20,10 +20,11 @@ def bonus
    }
   }
 
-  #code your solution here:
 
-  
+  epic_tragedy [:montague][:hero][:status] = "dead"
+  epic_tragedy [:capulet][:heroine][:status] = "dead"
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
+
 end
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,5 +1,21 @@
 def second_challenge
   epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
+  
+enddef second_challenge
+  epic_tragedy = {
     :montague => {
       #your key/value pairs here!
       }, 
